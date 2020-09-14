de3f78ffd2b7ed48034dfe0c621e2b37c0cb2716
kakou34 <kaouther.mouheb@gmail.com>
2020-09-14 13:41:11 +0300

    Log out button added

--- lib/Commun/Drawer.dart
+++ lib/Commun/Drawer.dart
@@ -1,4 +1,6 @@
+import 'package:eys/Globals.dart';
 import 'package:eys/Routes/Routes.dart';
+import 'package:eys/main.dart';
 import 'package:flutter/material.dart';
 
 class AppDrawer extends StatelessWidget {
@@ -25,6 +27,16 @@ class AppDrawer extends StatelessWidget {
           ListTile(
             title: Text('Available Events'),
             onTap: () => Navigator.pushReplacementNamed(context, Routes.availableEvents),
+          ),
+          ListTile(
+            title: Text('Log out'),
+            onTap: () => {
+              storage.delete(key: "user"),
+              Globals.isLoggedIn = false,
+              Globals.token ="",
+              Globals.currentUser = null,
+              Navigator.pushReplacementNamed(context, Routes.login),
+            },
           ),
         ],
       ),
--- lib/Globals.dart
+++ lib/Globals.dart
@@ -5,7 +5,7 @@ import 'package:eys/Authentication/UserDetails.dart';
 
 
 class Globals {
-  static const SERVER_IP = 'http://713a25c1206d.ngrok.io';
+  static const SERVER_IP = 'http://624156ed9df9.ngrok.io';
   static String token = "";
   static UserDetails currentUser = null;
   static bool isLoggedIn = false;
